5f80fbe5b6e26b73d0129a3313c9f44abc4b195e
Leo Lebtahi <lebtahileo@gmail.com>
2026-02-23 17:13:30 +0100

    Create a C program that prints exactly a sentence followed by a new line

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints a sentence
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle\n)
+	return 0;
+}
