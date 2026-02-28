f39fbd87bed26d8022bfaa3d98cb9dfb5c9e3f4a
Leo Lebtahi <lebtahileo@gmail.com>
2026-02-28 20:25:07 +0100

    Adjusted to betty coding style

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,19 @@
  */
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i++)      /* chaque ligne */
-    {
-        for (j = 0; j < size; j++)  /* chaque caractère '#' */
-            _putchar('#');
+		for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		_putchar('#');
 
-        _putchar('\n');             /* nouvelle ligne */
-    }
+		_putchar('\n');
+	}
 }
