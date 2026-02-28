07f64ca91b724acde8d529947fcfe0b4cff41f07
Leo Lebtahi <lebtahileo@gmail.com>
2026-02-28 18:09:10 +0100

    Adjusted the place of the 'for' clause for better compilation

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,14 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
+		if (n <= 0)
+		{
+			_putchar('\n');
+			return;
+		}
 
 	for (i = 0; i < n; i++)
 		_putchar('_');
 
-		_putchar('\n');
+	_putchar('\n');
 }
