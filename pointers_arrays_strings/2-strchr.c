2590430cfb9c93329f70f26bd2ab13a941d71580
Leo Lebtahi <lebtahileo@gmail.com>
2026-03-08 15:56:30 +0100

    Adjusted the code to the betty style of coding - added comment as well

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,36 @@
 #include "main.h"
 
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to search for
+ *
+ * Description: This function searches for the first occurrence
+ * of the character c in the string s, including the terminating
+ * null byte ('\0'). If the character is found, a pointer to the
+ * first occurrence of the character in the string is returned.
+ * If the character is not found, the function returns NULL.
+ *
+ * Return: pointer to the first occurrence of c in s, or NULL
+ */
+
 char *_strchr(char *s, char c)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-        {
-            return (&s[i]);
-        }
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
 
-    if (s[i] == c)
-    {
-        return (&s[i]);
-    }
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
 
-    return (0);
+	return (0);
 }
