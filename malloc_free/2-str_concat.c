4083eafb2143ba1d6854b8933b3414dc173449a9
Leo Lebtahi <lebtahileo@gmail.com>
2026-03-11 12:21:13 +0100

    Change the value of s1 and s2 if they're NULL

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
