0a9d6691625c7a78ccc5c550dcd65123c60fa962
Leo Lebtahi <lebtahileo@gmail.com>
2026-03-17 16:31:31 +0100

    Fixed compilation problems

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -36,27 +36,6 @@ char *_strcpy(char *dest, char *src)
 	}
 
 	dest[i] = '\0';
-/**
- * _strcpy - copies a string
- * @dest: destination
- * @src: source
- *
- * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
-}
 	return (dest);
 }
 
