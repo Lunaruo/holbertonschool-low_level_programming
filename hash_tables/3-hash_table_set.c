ed0425475ac4862a49604b0e1166fd466994ba00
Leo Lebtahi <lebtahileo@gmail.com>
2026-04-04 18:03:38 +0200

    Updated the file to make it Betty compliant by splitting it into multiple function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,39 +3,39 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table
- * @key: key (cannot be empty)
- * @value: value associated with key
+ * update_value - updates value of an existing key
+ * @node: node to update
+ * @value: new value
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: 1 if success, 0 if fail
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int update_value(hash_node_t *node, const char *value)
 {
-	hash_node_t *new, *node;
-	unsigned long int index;
-	char *dup_value;
+	char *dup;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	dup = strdup(value);
+	if (dup == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
+	free(node->value);
+	node->value = dup;
 
-	while (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-		{
-			dup_value = strdup(value);
-			if (dup_value == NULL)
-				return (0);
+	return (1);
+}
 
-			free(node->value);
-			node->value = dup_value;
-			return (1);
-		}
-		node = node->next;
-	}
+/**
+ * add_new_node - adds a new node at index
+ * @ht: hash table
+ * @index: index to insert
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if success, 0 if fail
+ */
+int add_new_node(hash_table_t *ht, unsigned long int index,
+		 const char *key, const char *value)
+{
+	hash_node_t *new;
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
@@ -60,4 +60,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = new;
 
 	return (1);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key (cannot be empty)
+ * @value: value associated with key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (update_value(node, value));
+		node = node->next;
+	}
+
+	return (add_new_node(ht, index, key, value));
 }
